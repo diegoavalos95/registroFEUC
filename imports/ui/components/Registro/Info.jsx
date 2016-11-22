02e445bd58b1147f5ec4471dc0543adb47937150
alexisllamas <alexisnavarro23@gmail.com>
2016-11-22 06:58:37 +0000

    Add flyer to info page

--- imports/ui/components/Registro/Info.jsx
+++ imports/ui/components/Registro/Info.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default class Info extends React.Component {
 	render() {
-		return <div>Soy la info</div>
+		return <img src="/flyer.jpg" alt="Flyer" height="100%" width="100%"/>
 	}
 }
