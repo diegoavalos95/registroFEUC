560b5cd6f568809d0f3a7249467358b76e0c9b83
alexisllamas <alexisnavarro23@gmail.com>
2016-11-22 08:15:32 +0000

    Put the image :D

--- imports/ui/components/Registro/Info.jsx
+++ imports/ui/components/Registro/Info.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default class Info extends React.Component {
 	render() {
-		return <img src="/flyer.jpg" alt="Flyer" height="100%" width="100%"/>
+		return <img style={{marginTop: '26px'}}src="/flyer.jpg" alt="Flyer" height="100%" width="100%"/>
 	}
 }
